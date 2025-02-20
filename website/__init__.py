e7d956d62ac401b8944fdb4715d089fabbbb619d
Dominik Krenn <dominik.krenn@1tool.com>
2025-02-20 14:55:36 +0100

    add 404 error handler to render custom 404 page

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
@@ -6,5 +6,9 @@ def create_app():
     from .routes.public import public
     
     app.register_blueprint(public)
+    
+    @app.errorhandler(404) 
+    def not_found(e): 
+        return render_template("404.html") 
     
     return app
