eed683b925c2d0ac2d09f6c0282fe3076fdfdc34
Dominik Krenn <dominik.krenn@1tool.com>
2025-02-20 13:35:09 +0100

    navbar layout html + routes shells to prevent templating error

--- website/routes/public.py
+++ website/routes/public.py
@@ -5,3 +5,19 @@ public = Blueprint('public', __name__, template_folder='templates')
 @public.route('/')
 def index():
     return render_template('index.html')
+
+@public.route('/about')
+def about():
+    return render_template('index.html')
+
+@public.route('/tags')
+def tags():
+    return render_template('index.html')
+
+@public.route('/recipes')
+def recipes():
+    return render_template('index.html')
+
+@public.route('/contact')
+def contact():
+    return render_template('index.html')
