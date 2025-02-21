3ddce26961ae57c57d49231f39ddc2305530acd4
lordlogo2002 <i96774080@gmail.com>
2025-02-21 18:58:12 +0100

    Add authentication blueprint and register with app

--- website/__init__.py
+++ website/__init__.py
@@ -5,10 +5,12 @@ def create_app():
     
     from .routes.public import public
     from .routes.api import api
+    from .routes.auth import auth
     
-    app.register_blueprint(public)
+    app.register_blueprint(public, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(api, url_prefix='/api')
-    
+
     @app.errorhandler(404) 
     def not_found(e): 
         return render_template("404.html") 
